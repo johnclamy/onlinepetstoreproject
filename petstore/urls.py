1789efe48ae964f641d16b2170dbcf68aef6e682
johnclamy <johnclamy@gmail.com>
2026-04-15 22:01:30 +0100

    include django_browser_reload url in the root urls.py

--- petstore/urls.py
+++ petstore/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    # Include django_browser_reload URLs only in DEBUG mode
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
